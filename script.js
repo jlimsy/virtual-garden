10ff64759315142008c428e752fc69271f72ec06
jlimsy <limsy.joey@gmail.com>
2025-03-14 08:21:03 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("garden");
+const ctx = canvas.getContext("2d");
+
+const img = new Image();
+img.src = "https://www.w3schools.com/tags/img_girl.jpg";
+
+// img.onload = function () {
+//   ctx.drawImage(img, 0, 0);
+// };
